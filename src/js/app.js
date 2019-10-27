2c6e75bcfafe182dfc163b94047567fe9763e206
Sergey Eromchenko <40355944+flunk72@users.noreply.github.com>
2019-10-27 10:24:49 +0500

    add js and others

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,33 @@
 if (module.hot) {
   module.hot.accept();
 }
+
+// Фильтр
+let filter = () => {
+  let input = document.getElementById('tract');
+
+  input.addEventListener('keyup', () => {
+    let filter = input.value.toLowerCase(),
+        filterItems = document.querySelectorAll('#search label');
+        console.log(filterItems)
+        filterItems.forEach(item => {
+          if(item.innerHTML.toLowerCase().indexOf(filter) > -1) {
+            item.style.display = '';
+          } else {
+            item.style.display = 'none'
+          }
+        })
+  })
+};
+
+filter();
+
+// Сброс чекбоксов
+
+document.getElementById('clear').addEventListener('click', (e) => {
+    let block = document.getElementById('tractItems');
+    let kkk = block.querySelectorAll('input[type=checkbox]');
+    kkk.forEach(function(el) {
+      el.checked = false;
+    });
+})
